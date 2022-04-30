85da9193025514c57bf654223c9f74d1d0758a78
mkoohaki <76530864+mkoohaki@users.noreply.github.com>
2022-04-30 08:43:35 -0400

    First commit

--- src/test/java/com/walmart/WalmartApplicationTests.java
+++ src/test/java/com/walmart/WalmartApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.walmart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class WalmartApplicationTests {
 
 	@Test
 	void contextLoads() {
