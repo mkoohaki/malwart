08cb6aecce7e9b074d74d1a7da3b22b110de79d1
mkoohaki <76530864+mkoohaki@users.noreply.github.com>
2022-05-02 17:32:54 -0400

    Created token package

--- src/main/java/com/walmart/appuser/AppUserService.java
+++ src/main/java/com/walmart/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.walmart.appuser;
 
+import com.walmart.registration.token.ConfirmationToken;
+import com.walmart.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +21,7 @@ public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -31,6 +37,22 @@ public class AppUserService implements UserDetailsService {
         appUser.setPassword(encodedPassword);
 
         appUserRepository.save(appUser);
-        return "It works";
+
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+
+
+        return token;
+    }
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
     }
 }
--- src/main/java/com/walmart/registration/RegistrationService.java
+++ src/main/java/com/walmart/registration/RegistrationService.java
@@ -3,8 +3,14 @@ package com.walmart.registration;
 import com.walmart.appuser.AppUser;
 import com.walmart.appuser.AppUserRole;
 import com.walmart.appuser.AppUserService;
+import com.walmart.registration.token.ConfirmationToken;
+import com.walmart.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
+import org.apache.tomcat.jni.Local;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
@@ -13,6 +19,7 @@ public class RegistrationService {
     private EmailValidator emailValidator;
     private final static String EMAIL_IS_NOT_VALID_MESSAGE = "Email is not valid";
     private final AppUserService appUserService;
+    private ConfirmationTokenService confirmationTokenService;
 
     public String register(RegistrationRequest request) {
 
@@ -30,5 +37,28 @@ public class RegistrationService {
                         AppUserRole.USER
                 )
         );
+    }
+
+    @Transactional
+    public String confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService
+                .getToken(token)
+                .orElseThrow(() ->
+                        new IllegalStateException("token not found"));
+
+        if (confirmationToken.getConfirmedAt() != null) {
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if (expiredAt.isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        appUserService.enableAppUser(
+                confirmationToken.getAppUser().getEmail());
+        return "confirmed";
     }
 }
